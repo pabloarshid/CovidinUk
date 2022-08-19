4f70e139ca475600eb98440e5150e84b60eaaf14
pabloarshid <umarsa.birvancity@gmail.com>
2022-08-19 14:01:46 -0700

    moved options to main extensions part, can now display each countries stats when option button is clicked

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
 
+let page = document.getElementById("buttonDiv");
+const presetAreas = ["england", "northern ireland", "scotland", "wales"];
+let selectedClassName = "current";
+
+
+// Reacts to a button click by marking the selected button and saving
+// the selection
+function handleButtonClick(event) {
+  // Remove styling from the previously selected color
+  let current = event.target.parentElement.querySelector(
+    `.${selectedClassName}`
+  );
+  if (current && current !== event.target) {
+    current.classList.remove(selectedClassName);
+  }
+
+  // Mark the button as selected
+  let areaName = event.target.dataset.area;
+  event.target.classList.add(selectedClassName);
+  chrome.storage.sync.set({ areaName });
+  chrome.storage.sync.get("areaName", (data) => {
+    console.log("clearing table")
+	  var Table =document.getElementById("table");
+    Table.innerHTML = "";
+    fetchData(data.areaName);
+  });
+}
+
+function constructOptions(areaNames) {
+  chrome.storage.sync.get("areaName", (data) =>{
+    let currentarea = data.areaName;
+    for (let areanames1 of areaNames){
+      let button = document.createElement("button")
+      button.dataset.area = areanames1
+      button.innerText = areanames1
+      // …mark the currently selected color…
+      if (areanames1 === currentarea) {
+        button.classList.add(selectedClassName);
+      }
+      // …and register a listener for when that button is clicked
+      button.addEventListener("click", handleButtonClick);
+      page.appendChild(button);
+    }
+  });
+}
+
+constructOptions(presetAreas);
+
 async function fetchData(areanamed) {
     const res=await fetch ('https://api.coronavirus.data.gov.uk/v1/data?' +
     'filters=areaType=nation;' + 'areaName=' + areanamed+'&' +
@@ -13,11 +61,4 @@ async function fetchData(areanamed) {
           cell.innerHTML = item[key];
       }
     }
-    // document.getElementById("date").innerHTML=record.data.date;
-    // document.getElementById("newCasesByPublishDate").innerHTML=record.data.newCases;
-
 }
-
-chrome.storage.sync.get("areaName", (data) => {
-  fetchData(data.areaName);
-});
